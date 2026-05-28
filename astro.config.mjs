0f4ad3f70c0da65e70309c18cc21677b292fac65
大塚崚平 <ryo@ootsukanoMacBook-Air.local>
2026-05-28 16:21:33 +0900

    remove github pages adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import githubPages from '@astrojs/github-pages';
 
 export default defineConfig({
   site: 'https://ryo780803.github.io',
   base: '/project1/',
-  adapter: githubPages(),
+
   vite: {
     plugins: [tailwindcss()],
   },
