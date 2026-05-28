8ccb83752d912d73a3618312f9424ebcb661db0b
大塚崚平 <ryo@ootsukanoMacBook-Air.local>
2026-05-28 16:14:47 +0900

    fix github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import githubPages from '@astrojs/github-pages';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://ryo780803.github.io',
+  base: '/project1/',
+  adapter: githubPages(),
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
